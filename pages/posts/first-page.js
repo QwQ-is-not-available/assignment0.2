abbb139370b8dcbbb9d1f3e8bccdb03a8acf084f
QwQ-is-not-available <omga2333@gmail.com>
2023-04-17 16:14:52 +0900

    for assignment 0.3

--- pages/posts/first-page.js
+++ pages/posts/first-page.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image'
 
 export default function FirstPage() {
   return (
@@ -7,6 +8,13 @@ export default function FirstPage() {
       <h2>Name: Nan Haixu</h2>
       <p>I have a mobile phone.</p>
       <br></br>
+      <p>task2:</p>
+      <Image src="/task2.png" width={640*0.7} height={360*0.7}/>
+      <br></br>
+      <p>task3:</p>
+      <Image src="/task3.png" width={640*0.7} height={360*0.7}/>
+      <br>
+      </br>
       <h3>
         <Link href="/">Back to home</Link>
       </h3>
